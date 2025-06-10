ebfec8f05ac02e1a9a47c4d4a552927533ded296
fabian <113838982+savaqe21@users.noreply.github.com>
2025-06-10 18:46:53 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Minimal JS challenge – obfuskowana wiadomość
-let payload = atob("RkxBR3tzZW5kX3RoYXRfY2hhaXJ9");
-console.log("Decoded transmission: " + payload);
-
-// Easter egg
-console.log("Pssst... check lezak.png closely 👀");
